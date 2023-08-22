b6f00d1bb0d1330b9ac8ad8c86903643d0be2222
anjeongkyun <dkswjdrbs12@naver.com>
2023-08-23 00:05:21 +0900

    헬스체킹을 위한 API를 등록한다

---
+++ api/src/main/kotlin/org/tagwonder/controllers/HealthCheckController.kt
@@ -0,0 +1,11 @@
+package org.tagwonder.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+    @GetMapping("/")
+    fun healthCheck() {
+    }
+}
