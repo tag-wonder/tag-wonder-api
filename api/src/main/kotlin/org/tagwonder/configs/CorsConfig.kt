12740e09d8a9abf674bf5e8affe1d9d2efebd8da
anjeongkyun <dkswjdrbs12@naver.com>
2023-08-28 23:21:09 +0900

    cors 설정을 추가한다

---
+++ api/src/main/kotlin/org/tagwonder/configs/CorsConfig.kt
@@ -0,0 +1,15 @@
+package org.tagwonder.configs
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+open class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+    }
+}
