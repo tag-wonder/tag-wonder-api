881e82ad9b7a1e9a08e534f3909b13e7b947d084
anjeongkyun <dkswjdrbs12@naver.com>
2023-09-15 01:14:36 +0900

    health check api의 응답값을 명시한다

--- api/src/main/kotlin/org/tagwonder/controllers/HealthCheckController.kt
+++ api/src/main/kotlin/org/tagwonder/controllers/HealthCheckController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HealthCheckController {
     @GetMapping("/")
-    fun healthCheck() {
+    fun healthCheck(): String {
+        return "OK"
     }
 }
