c59e2554e05a0f3c5078e47ea24ecde998b6a9bf
anjeongkyun <dkswjdrbs12@naver.com>
2023-09-19 00:57:26 +0900

    api module의 gradle에 jwt dependency를 추가한다

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,8 +8,9 @@ project(":api") {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter")
         implementation("org.springframework.boot:spring-boot-starter-web")
-        implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-        implementation("org.springframework.boot:spring-boot-starter-security")
+        implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+        implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+        implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
         runtimeOnly("com.mysql:mysql-connector-j")
 
