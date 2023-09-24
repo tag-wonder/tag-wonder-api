74b67f3c07b598a79df7ceafd66a495c12aae88f
anjeongkyun <dkswjdrbs12@naver.com>
2023-09-24 22:17:10 +0900

    getTagsQueryProcessor의 API Controller를 등록한다

--- api/src/main/kotlin/org/tagwonder/controllers/TagController.kt
+++ api/src/main/kotlin/org/tagwonder/controllers/TagController.kt
@@ -2,16 +2,22 @@ package org.tagwonder.controllers
 
 import com.healingpaper.solution.domain.exceptions.InvalidCommandException
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.tagwonder.commands.CreateTagsCommand
 import org.tagwonder.exceptions.toHttpException
+import org.tagwonder.queries.GetTagsQuery
+import org.tagwonder.queries.GetTagsQueryResponse
 import org.tagwonder.usecases.commands.CreateTagsCommandExecutor
+import org.tagwonder.usecases.queries.GetTagsQueryProcessor
 
 @RestController
 class TagController(
-    private val createTagsCommandExecutor: CreateTagsCommandExecutor
+    private val createTagsCommandExecutor: CreateTagsCommandExecutor,
+    private val getTagsQueryProcessor: GetTagsQueryProcessor
 ) {
     @PostMapping("/tags")
     fun createTags(
@@ -24,5 +30,16 @@ class TagController(
         } catch (e: InvalidCommandException) {
             throw e.toHttpException(HttpStatus.BAD_REQUEST)
         }
+    }
+
+    @GetMapping("/members/{memberId}/tags")
+    fun getTags(
+        @PathVariable("memberId") memberId: Long
+    ): GetTagsQueryResponse{
+        return getTagsQueryProcessor.process(
+            GetTagsQuery(
+                memberId = memberId
+            )
+        )
     }
 }
